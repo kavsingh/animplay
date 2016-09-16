05cfdfedc126b8a7e8bf65549f07c589b68fb6d7
kav singh <flakisms@gmail.com>
2016-09-16 14:24:16 +0200

    some updates

--- src/loaders.test.js
+++ src/loaders.test.js
@@ -1,20 +1,17 @@
 import test from 'ava';
+import sinon from 'sinon';
 import { loadAnimations } from './loaders';
 
 test('Loads animations', async t => {
   t.plan(2);
 
-  let loadCount = 0;
-  const mockLoader = name => {
-    loadCount += 1;
-    return Promise.resolve({ name });
-  };
-
+  const spy = sinon.spy();
+  const mockLoader = name => Promise.resolve().then(spy).then(() => ({ name }));
   const loaded = await loadAnimations(['this', 'that'], mockLoader);
 
-  t.is(loadCount, 2);
-  t.deepEqual(loaded, {
+  t.is(2, spy.callCount);
+  t.deepEqual({
     this: { name: 'this' },
     that: { name: 'that' },
-  });
+  }, loaded);
 });
